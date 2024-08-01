614cd7a0093c72d8d7c05e7591d8dc9ed8b7709b
Bet365 RE JS <re.bet365.com@gmail.com>
2024-08-01 17:17:56 +1000

    initial code commit

---
+++ mitmproxy.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+mitmproxy -s mitmproxy/src/python/download-payload.py
---
+++ new-conda-environment.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+conda create -n bet365-re-js python=3.11 && \
+conda activate bet365-re-js
+
+./python-dependencies.sh
