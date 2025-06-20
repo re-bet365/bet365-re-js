0d03459988675abc9a8786cbeee39e20da603bb0
Bet365 RE JS <re.bet365.com@gmail.com>
2025-06-21 01:41:17 +1000

    deobfuscated keywords

--- mitmproxy/src/javascript/refactor-new-obfuscated-code-jscodeshift-2.js
+++ mitmproxy/src/javascript/refactor-new-obfuscated-code-jscodeshift-2.js
@@ -1,4 +1,5 @@
 import {AstTransformer} from "./refactor-obfuscated-code-jscodeshift-common";
+import j from "jscodeshift";
 
 class KeywordTransformer extends AstTransformer {
     constructor(stepNumber, jscodeshiftAst, output, outputBaseName) {
@@ -6,21 +7,14 @@ class KeywordTransformer extends AstTransformer {
     }
 
     performTransform() {
-        const variableDeclarations = this.jscodeshiftAst.find(this.jscodeshiftAst.VariableDeclaration)
-            .for(varDecl => {
-                console.log(varDecl);
-            });
-        this.jscodeshiftAst.find(this.jscodeshiftAst.VariableDeclaration)
+        const keywords = this.jscodeshiftAst.find(j.VariableDeclaration)
             .filter(path => {
                 const declarations = path.node.declarations;
                 if (declarations.length === 0) return false;
-                const init = declarations[0].init;
-                const id = declarations[0].id;
-                return init && init.type === 'ArrayExpression' && init.elements.length > 0 && init.elements.every(el => el.type === 'StringLiteral') && id.type === 'Identifier' && id.name.startsWith('_0x');
+                const initialValue = declarations[0].init;
+                const identifier = declarations[0].id;
+                return initialValue && initialValue.type === "ArrayExpression" && initialValue.elements.length > 0 && initialValue.elements.every(el => el.type === "StringLiteral" || el.type === "Literal") && identifier.type === "Identifier";
             })
-            .forEach(path => {
-                path.node.declarations[0].id = root.jscodeshift.identifier('keywords');
-            });
 
         return this.jscodeshiftAst;
     }
